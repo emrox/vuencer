26dea8183f800c8da719bc71616e210231bd3d76
Stefan Bauckmeier <stefan@bauckmeier.de>
2017-03-04 09:57:44 +0100

    feat: render basic step sequencer triggers

---
+++ src/components/SequencerTrigger.vue
@@ -0,0 +1,48 @@
+<template lang="jade">
+  div.sequencer-trigger
+    div.sequencer-trigger__button
+    div.sequencer-trigger__state
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+  .sequencer-trigger {
+    display: inline-block;
+    margin: 0.5em;
+
+    &__button {
+      width: 2em;
+      height: 2em;
+      border: 1px solid #999;
+      border-radius: 50%;
+      background-color: #000;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #333;
+      }
+
+      &:active {
+        border-color: #333;
+      }
+    }
+
+    &__state {
+      margin: 0.4em auto 0;
+      width: 0.4em;
+      height: 0.4em;
+      border: 1px solid #f99;
+      border-radius: 50%;
+      background-color: #fee;
+      animation: sequencer-trigger__state__blink 1s steps(2, start) infinite;
+    }
+  }
+
+  @keyframes sequencer-trigger__state__blink {
+    to {
+      background-color: #f00;
+    }
+  }
+</style>
---
+++ src/components/StepSeq.vue
@@ -0,0 +1,20 @@
+<template lang="jade">
+  div.step-seq
+    span( v-for="n in 16" )
+      sequencer-trigger
+</template>
+
+<script>
+  import SequencerTrigger from './SequencerTrigger.vue'
+
+  export default {
+    components: { SequencerTrigger },
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
