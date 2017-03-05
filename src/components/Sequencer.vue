f1520dd6248e62a85b2ef76751c8277db42fc74f
Stefan Bauckmeier <stefan@bauckmeier.de>
2017-03-05 15:13:27 +0100

    feat: own sequencer component
    for better modularity

---
+++ src/components/Sequencer.vue
@@ -0,0 +1,26 @@
+<template lang="jade">
+  .sequencer
+    span( v-for="sequenceRowNumber in rows" )
+      sequencer-row( :sequence = "sequenceRowNumber", :steps = "steps" )
+</template>
+
+<script>
+  import SequencerRow from './SequencerRow.vue';
+
+  export default {
+    components: { SequencerRow },
+    props: {
+      rows: {
+        type: Number,
+        default: 4,
+      },
+      steps: {
+        type: Number,
+        default: 16,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+</style>
--- src/components/StepSeq.vue
+++ src/components/StepSeq.vue
@@ -1,16 +1,15 @@
 <template lang="jade">
   div.step-seq
     tempo
-    span( v-for="sequence in 4" )
-      sequencer-row( :sequence = "sequence", :steps = 16 )
+    sequencer
 </template>
 
 <script>
-  import SequencerRow from './SequencerRow.vue';
+  import Sequencer from './Sequencer.vue';
   import Tempo from './Tempo.vue';
 
   export default {
-    components: { SequencerRow, Tempo },
+    components: { Sequencer, Tempo },
   };
 </script>
 
