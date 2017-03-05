bbce05bcbea49f17d4dd8a3b5b7a73ac26b42222
Stefan Bauckmeier <stefan@bauckmeier.de>
2017-03-05 10:07:51 +0100

    feat: changeable tempo
    with data binding

--- src/components/Tempo.vue
+++ src/components/Tempo.vue
@@ -1,7 +1,7 @@
 <template lang="jade">
   .tempo
     .tempo__display
-      {{ tempo }}
+      input.tempo__input( type="number", v-model="tempo", number, lazy, min="30", max="250", step="1" )
     led
 </template>
 
@@ -34,6 +34,25 @@
       font-family: 'VT323', monospace;
       font-size: 2em;
       color: #fff;
+    }
+
+    &__input {
+      width: 2em;
+      border: 0;
+      font-family: 'VT323', monospace;
+      font-size: 2em;
+      text-align: right;
+      color: #fff;
+      background-color: transparent;
+      outline: none;
+
+      // turn off spin box / arrows, http://stackoverflow.com/questions/3790935/can-i-hide-the-html5-number-input-s-spin-box
+      -moz-appearance: textfield;
+      &::-webkit-outer-spin-button,
+      &::-webkit-inner-spin-button {
+        margin: 0;
+        -webkit-appearance: none;
+      }
     }
 
     .led {
