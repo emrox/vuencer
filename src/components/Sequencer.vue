42f9b2bdc6fb8a909cdbdfa2b74fdc851950d067
Stefan Bauckmeier <stefan@bauckmeier.de>
2017-03-16 21:28:26 +0100

    feat: easier & more flexible sequencer rendering
    by removing SequencerRow component and making defining number of steps
    and max. steps per row much easier.

--- src/components/Sequencer.vue
+++ src/components/Sequencer.vue
@@ -1,32 +1,42 @@
 <template lang="jade">
   .sequencer
-    span( v-for="sequenceRowNumber in rows" )
-      sequencer-row( :sequence = "sequenceRowNumber", :steps = "steps" )
+    .sequencer-row( v-for="rowNumber in rowCount" )
+      sequencer-trigger( v-for="step in stepCountOnRow(rowNumber)", :key = "step", :step = "step" )
 </template>
 
 <script>
   import EventBus from '../libs/EventBus';
-  import SequencerRow from './SequencerRow.vue';
+  import SequencerTrigger from './SequencerTrigger.vue';
 
   export default {
-    components: { SequencerRow },
+    components: { SequencerTrigger },
     props: {
-      rows: {
-        type: Number,
-        default: 4,
-      },
       steps: {
+        type: Number,
+        default: 64,
+      },
+      maxStepsPerRow: {
         type: Number,
         default: 16,
       },
     },
     data() {
       return {
-        stepCount: 0,
+        currentStep: 0,
         currentRow: 1,
       };
     },
+    computed: {
+      rowCount() {
+        return Math.ceil(this.steps / this.maxStepsPerRow);
+      },
+    },
     methods: {
+      stepCountOnRow(rowNumber) {
+        const self = this;
+        if (rowNumber < self.rowCount) { return self.maxStepsPerRow; }
+        return self.steps - ((self.rowCount - 1) * self.maxStepsPerRow);
+      },
       processTick(tickCount) {
         if (tickCount % 6 === 1) {
           this.$emit('step');
@@ -35,20 +45,12 @@
       processStep() {
         const self = this;
 
-        self.stepCount += 1;
-        if (self.stepCount > (self.rows * self.steps)) {
-          self.stepCount = 1;
+        self.currentStep += 1;
+        if (self.currentStep > self.steps) {
+          self.currentStep = 1;
         }
 
-        const currentRowStep = (self.stepCount % self.steps) + 1;
-        if (currentRowStep === 1) {
-          self.currentRow += 1;
-          if (self.currentRow > 4) {
-            self.currentRow = 1;
-          }
-        }
-
-        self.$children[self.currentRow - 1].$emit('step', currentRowStep);
+        self.$children[self.currentStep - 1].$emit('trigger');
       },
     },
     created() {
